b9bceb27b3b5134a9a088952f9dfb602fea57147
mhurtgen <mhurtgen@gmail.com>
2024-08-17 09:39:14 +0200

    Add files via upload

--- testscraper.py
+++ testscraper.py
@@ -6,6 +6,7 @@ import numpy as np
 import re
 
 works=list()
+next='null'
 allworks_composer=list()
 url='https://imslp.org/wiki/Category:Mozart,_Wolfgang_Amadeus'
 conn = sqlite3.connect('ISMLPperiodtotal2')
@@ -27,71 +28,73 @@ def geturl(name):
     
     return url
 
-def gethrefnext(bs_obj):
-    for el in bs_obj.select('a'):
-        data=el.text
-        
-        test=next_pattern.match(data)
-        if (test!=None):
-            href=el['href']
-#        else:
-#            href=None
-    return href
-
-
-
 def getlinks(url):
-    """get url"""
     
-   
     """download html from url"""
     webcontent=requests.get(url)
     """parse downloaded html"""
     bs_obj = BeautifulSoup(webcontent.text,"html.parser")
 
+    #links=bs_obj.findAll('a',attrs={'id':'mw-pages'})
     tag=bs_obj.find(id="mw-pages")
     links = tag.findAll('a')
     
+    #next=tag.findAll(lambda tag:'text' in tag.text)
+    
     return links
+
     #return bs_obj
 
 def getlistofworks(links):
+    global next
     """get all titles on page"""
     for link in links:
+       
         if ('next' not in link.text):
             works.append(link.text)
         else:
-            next=link['href']
-    return works, next
-
+            
+                next=link['href']
+               
+           
+           
+    
+    return next
+            
 def getallworks_page(url):
     """get works on page"""
     links=getlinks(url)
-
-    works,next=getlistofworks(links)
-    print(next)
-    return works, next
+    next=getlistofworks(links)
+    
+    return next
 
 def getallworks(name):
-    
+    works=list()
     """loop over all pages listing works of composer"""
     name='Mozart,_Wolfgang_Amadeus'
     url=geturl(name)
+    next=getallworks_page(url)
+    next_pre=next
     
-    while True:
+    while (next!='null'):
         """get works on page"""
-        works,next=getallworks_page(url)
-        works.append(works)
-       
+        
         url='https://imslp.org'+next
+        next=getallworks_page(url)
+        #works.append(works)
+        if (next_pre==next):
+            break
+        next_pre=next
+        print(next)
+       
         
        
-    return works
+    return 
 
 name='Mozart,_Wolfgang_Amadeus'
 
 #list=getlinks(url
-def getallworkstot(conn):
+def getallworksallcomposers(conn):
     data=getdata(conn)
     
     
@@ -110,16 +113,11 @@ def getallworkstot(conn):
 #print(works)
 
 
-url=geturl(name)
-links=getlinks(url)
-
-works,next=getallworks_page(url)
-print(next)
-url='https://imslp.org'+next
-webcontent=requests.get(url)
-print(url)
-test=getlinks(url)
-print(test)
+#print(works)
+#getallworks_page(url)
+getallworks(name)
+#with open('w','testworks.txt') as f:
+#    f.write(works)
 #print(next)
 #print(next['href'])
 
